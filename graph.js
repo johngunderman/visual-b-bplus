856a227343af2c2e54cd7ed387f5007b1aa49f77
John Gunderman <johngunderman@gmail.com>
2012-04-04 21:30:14 -0400

    Add vertical lines based on degree of node

--- graph.js
+++ graph.js
@@ -3,6 +3,9 @@
 var layer = {};
 var stage = {};
 
+var ITEM_WIDTH = 60;
+var ITEM_HEIGHT = 40;
+
 window.onload = function() {
     initializeCanvas();
     generateNode(3);
@@ -26,29 +29,60 @@ function initializeCanvas() {
 // returns a handle to the node.
 function generateNode(degree) {
 
-    var rectX = stage.width / 2 - 50;
-    var rectY = stage.height / 2 - 25;
+    var w = degree * ITEM_WIDTH;
+
+    var rectX = stage.width / 2 - w / 2;
+    var rectY = stage.height / 4 - 25;
+
+    var group = new Kinetic.Group({
+        draggable: true
+    });
 
     var box = new Kinetic.Rect({
         x: rectX,
         y: rectY,
-        width: 100,
-        height: 50,
+        width: w,
+        height: ITEM_HEIGHT,
         fill: "#00D2FF",
         stroke: "black",
         strokeWidth: 4,
         draggable: true
     });
 
+    group.add(box)
+
+    var spacing = Math.floor(w / degree);
+
+    for (var i = 1; i < degree; i++) {
+        (function() {
+            var xcoord = Math.floor(rectX + spacing * i);
+            var line = new Kinetic.Shape({
+                drawFunc: function() {
+                    var context = this.getContext();
+                    context.beginPath();
+                    context.moveTo(xcoord, rectY);
+                    context.lineTo(xcoord, rectY + ITEM_HEIGHT);
+                    context.closePath();
+                    this.fillStroke();
+                },
+                fill: "#00D2FF",
+                stroke: "black",
+                strokeWidth: 4
+            });
+            group.add(line);
+        })();
+    }
+
     // add cursor styling
-    box.on("mouseover", function() {
+    group.on("mouseover", function() {
         document.body.style.cursor = "pointer";
     });
-    box.on("mouseout", function() {
+    group.on("mouseout", function() {
         document.body.style.cursor = "default";
     });
 
-    layer.add(box);
+
+    layer.add(group);
     stage.add(layer);
 }
 
